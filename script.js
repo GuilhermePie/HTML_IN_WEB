dc1fce54f3215dc267bfb993ac290292b7ba50b4
GuilhermePie <guilhermeSpiedade@gmail.com>
2025-01-13 23:24:47 -0300

    btn para esconder campos e design sendo feito

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const textArea = document.getElementById("textArea")
 const resultBox = document.getElementById("resultBox")
+const previewButton = document.getElementById("previewButton")
+const indexButton = document.getElementById("indexButton")
 
 textArea.addEventListener('input', ()=>{
     resultBox.innerHTML = `${textArea.value}`
 })
+
+previewButton.addEventListener('click', ()=>[
+    previewButton.classList.toggle("hideButton"),
+    resultBox.classList.toggle("hide")
+])
+
+indexButton.addEventListener('click', ()=>[
+    indexButton.classList.toggle("hideButton"),
+    textArea.classList.toggle("hide")
+])
+
